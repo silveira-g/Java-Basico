710e5dc8bde49bc81ed2eec3d3ce24d576be3e1e
silveira-g <silveira.alves.guilherme@gmail.com>
2022-11-19 13:14:20 -0300

    Exemplo de criação de variáveis

--- Tipos e Variáveis/tipos-e-variaveis/src/TiposVariaveis.java
+++ Tipos e Variáveis/tipos-e-variaveis/src/TiposVariaveis.java
@@ -1,5 +1,13 @@
 public class TiposVariaveis {
     public static void main(String[] args) {
-        
+        double salarioMinimo = 2500;
+        short numeroCurto = 1;
+        int numeroNormal = numeroCurto;
+        short numeroCurto2 = (short) numeroNormal;
+
+        int numero = 1;
+        numero = 2;
+        final double VALOR_DE_PI = 3.14;
+        System.out.println(VALOR_DE_PI + numero);
     }
 }
