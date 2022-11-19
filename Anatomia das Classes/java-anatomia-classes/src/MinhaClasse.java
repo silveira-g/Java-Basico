234647ffd00ee630b70fab4e2c3ff90f909e69fd
silveira-g <silveira.alves.guilherme@gmail.com>
2022-11-19 12:22:35 -0300

    Cria um método e o chama

--- Anatomia das Classes/java-anatomia-classes/src/MinhaClasse.java
+++ Anatomia das Classes/java-anatomia-classes/src/MinhaClasse.java
@@ -1,6 +1,11 @@
 public class MinhaClasse {
     public static void main(String[] args) {
-        System.out.println("Meu primeiro input");
-        
+  //      System.out.println("Meu primeiro input");
+        String primeiroNome = "Guilherme";
+        String segundoNome = "Silveira";
+        System.out.println(nomeCompleto(primeiroNome, segundoNome));
+    }
+    public static String nomeCompleto (String primeiroNome, String segundoNome){
+        return primeiroNome.concat(" ").concat(segundoNome);
     }
 }
