068af625b9bb77f3e137c1a87265646b7bf375a4
silveira-g <silveira.alves.guilherme@gmail.com>
2022-11-19 13:35:10 -0300

    Exemplos de operadores

---
+++ Tipos e Variáveis/tipos-e-variaveis/src/Operadores.java
@@ -0,0 +1,9 @@
+public class Operadores {
+    public static void main(String[] args) {
+        String nomeCompleto = "Linguagem" + "Java";
+        System.out.println(nomeCompleto);
+        boolean variavel = true;
+        System.out.println(variavel);
+        System.out.println(!variavel);
+    }
+}
