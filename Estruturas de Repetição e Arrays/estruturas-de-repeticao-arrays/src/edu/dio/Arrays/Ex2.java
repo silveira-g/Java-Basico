281f8b867e3e48517cad0084b924d135ceff8d11
silveira-g <silveira.alves.guilherme@gmail.com>
2022-11-19 18:27:11 -0300

    Resolucao exercicio 2 de Arrays

---
+++ Estruturas de Repetição e Arrays/estruturas-de-repeticao-arrays/src/edu/dio/Arrays/Ex2.java
@@ -0,0 +1,28 @@
+package edu.dio.Arrays;
+
+import java.util.Scanner;
+
+
+public class Ex2 {
+    public static void main(String[] args) {
+        //faça um programa que leia um vetor de consoantes de 6 caracters, e diga quantas consoantes foram lidas. imprima as consoates.
+        Scanner scan = new Scanner(System.in);
+        String[] consoantes = new String[6]; 
+        int contador=0;
+
+        for (int i = 0; i<consoantes.length; i++ ){ 
+            System.out.println("Escreva uma letra");
+            String letra = scan.next();
+            if ( !(letra.equalsIgnoreCase("a")|| 
+                (letra.equalsIgnoreCase("e")) ||
+                (letra.equalsIgnoreCase("i")) ||
+                (letra.equalsIgnoreCase("o")) ||
+                (letra.equalsIgnoreCase("u")))) 
+            consoantes[i] = letra;                      
+        }
+        for (int i = 0; i <consoantes.length; i++){ 
+            System.out.print(consoantes[i] + " ");
+        }
+        scan.close();
+    }
+}
